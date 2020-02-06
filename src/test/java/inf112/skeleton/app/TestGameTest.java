019ac90b8c3adf9b480237b6e618ae2419b96550
Alvar Hønsi <alvarhonsi@gmail.com>
2020-02-06 22:54:39 +0100

    Endret litt på struktur

--- src/test/java/inf112/skeleton/app/TestGameTest.java
+++ src/test/java/inf112/skeleton/app/TestGameTest.java
@@ -1,15 +1,35 @@
 package inf112.skeleton.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Vector2;
+import inf112.core.testingUtils.GdxTestRunner;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Unit test for TestGame
  */
+@RunWith(GdxTestRunner.class)
 public class TestGameTest
 {
+    TestGame game;
+
+    @Before
+    public void setUp() {
+        game = new TestGame();
+    }
+
     @Test
     public void PlayerDoesNotMoveIfMovementKeysAreNotPressed() {
+        float x = game.playerPosition.x;
+        float y = game.playerPosition.y;
+        game.keyDown(Input.Keys.SPACE);
+        assertEquals(x, game.playerPosition.x, 0.0001);
+        assertEquals(y, game.playerPosition.y, 0.0001);
 
     }
 
