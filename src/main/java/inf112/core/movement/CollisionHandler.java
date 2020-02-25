df2fcef05065d2f6d9454ae72905c0a49a272c27
Eskil Hamre Isaksen <eskilusken@gmail.com>
2020-02-25 12:17:46 +0100

    Respawn rotation fix + minor changes

--- src/main/java/inf112/core/movement/CollisionHandler.java
+++ src/main/java/inf112/core/movement/CollisionHandler.java
@@ -1,6 +1,7 @@
 package inf112.core.movement;
 
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import inf112.core.board.GameBoard;
 import inf112.core.player.Direction;
 import inf112.core.player.Player;
 
@@ -9,10 +10,14 @@ import java.util.List;
 public class CollisionHandler {
 
     private List<Player> players;
-    private TiledMapTileLayer playerLayer;
+    private GameBoard gameBoard;
 
-    public CollisionHandler(TiledMapTileLayer playerLayer) {
-        this.playerLayer = playerLayer;
+    public CollisionHandler(GameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    public CollisionHandler() {
+        this(new GameBoard());
     }
 
     public boolean add(Player player) {
