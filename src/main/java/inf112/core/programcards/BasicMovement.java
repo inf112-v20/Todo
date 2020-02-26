572fce36d5b279a197b7d2baebe81aab4dff06ed
Pål Henrik Hannus <paal_hannus@hotmail.com>
2020-02-26 17:33:26 +0100

    implemented a simple programcard class

---
+++ src/main/java/inf112/core/programcards/BasicMovement.java
@@ -0,0 +1,8 @@
+package inf112.core.programcards;
+
+public enum BasicMovement {
+    FORWARD,
+    BACKWARD,
+    ROTATE_LEFT,
+    ROTATE_RIGHT;
+}
---
+++ src/main/java/inf112/core/programcards/ProgramCard.java
@@ -0,0 +1,11 @@
+package inf112.core.programcards;
+
+import java.util.List;
+
+public class ProgramCard {
+    private List<BasicMovement> Sequence;
+    private int priority;
+    private String name;
+
+
+}
