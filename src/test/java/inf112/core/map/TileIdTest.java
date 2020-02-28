be70b393b20f68290dccebe11a524161f55ad34d
Pål Henrik Hannus <paal_hannus@hotmail.com>
2020-02-28 10:49:02 +0100

    Added tests for all wall attributes

--- src/test/java/inf112/core/map/TileIdTest.java
+++ src/test/java/inf112/core/map/TileIdTest.java
@@ -18,9 +18,54 @@ public class TileIdTest {
     }
 
     @Test
+    public void WALL_SOUTHShouldHaveAttributesSOUTH(){
+        Attributes attributes = TileId.WALL_SOUTH.getAttributes().get(0);
+        assertEquals(attributes, Attributes.SOUTH);
+    }
+
+    @Test
+    public void WALL_NORTHShouldHaveAttributesNORTH(){
+        Attributes attributes = TileId.WALL_NORTH.getAttributes().get(0);
+        assertEquals(attributes, Attributes.NORTH);
+    }
+
+    @Test
+    public void WALL_EASTShouldHaveAttributesEAST(){
+        Attributes attributes = TileId.WALL_EAST.getAttributes().get(0);
+        assertEquals(attributes, Attributes.EAST);
+    }
+
+    @Test
+    public void WALL_WESTShouldHaveAttributesWEST(){
+        Attributes attributes = TileId.WALL_WEST.getAttributes().get(0);
+        assertEquals(attributes, Attributes.WEST);
+    }
+
+    @Test
     public void WALL_SOUTH_WESTShouldHaveAttributesSOUTHAndWEST() {
         List<Attributes> attributes = TileId.WALL_SOUTH_WEST.getAttributes();
         for(Attributes attribute : attributes)
             assert(attribute.equals(Attributes.WEST) || attribute.equals(Attributes.SOUTH));
+    }
+
+    @Test
+    public void WALL_NORTH_WESTShouldHaveAttributesNORTHAndWEST() {
+        List<Attributes> attributes = TileId.WALL_NORTH_WEST.getAttributes();
+        for(Attributes attribute : attributes)
+            assert(attribute.equals(Attributes.WEST) || attribute.equals(Attributes.NORTH));
+    }
+
+    @Test
+    public void WALL_NORTH_EASTShouldHaveAttributesNORTHAndEAST() {
+        List<Attributes> attributes = TileId.WALL_NORTH_EAST.getAttributes();
+        for(Attributes attribute : attributes)
+            assert(attribute.equals(Attributes.EAST) || attribute.equals(Attributes.NORTH));
+    }
+
+    @Test
+    public void WALL_SOUTH_EASTShouldHaveAttributesSOUTHAndEAST() {
+        List<Attributes> attributes = TileId.WALL_SOUTH_EAST.getAttributes();
+        for(Attributes attribute : attributes)
+            assert(attribute.equals(Attributes.EAST) || attribute.equals(Attributes.SOUTH));
     }
 }
