3e950ade2a1ba1761b98de81c087b61dbcedc987
Pål Henrik Hannus <paal_hannus@hotmail.com>
2020-03-06 11:34:53 +0100

    Some essential deck methods

--- src/main/java/inf112/core/player/Deck.java
+++ src/main/java/inf112/core/player/Deck.java
@@ -4,19 +4,53 @@ import inf112.core.programcards.Card;
 import inf112.core.programcards.ProgramCard;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
     private List<ProgramCard> discardDeck = new ArrayList<>();
     private List<ProgramCard> activeDeck = new ArrayList<>();
-    private ProgramCard[] selectedCards = new ProgramCard[5];
-    // Skal være et felt i player
 
     public Deck(){
     }
 
     public void addCard(ProgramCard card) {
-
+        activeDeck.add(card);
     }
+
+    public void addCards(List<ProgramCard> cards){
+        for (ProgramCard card : cards){
+            activeDeck.add(card);
+        }
+    }
+
+    public void shuffleActive(){
+        Collections.shuffle(activeDeck);
+    }
+
+    // Returns list of cards for player to choose from
+    // Should throw exception if amount is greater than (activecards + discardcards)
+    public List<ProgramCard> getSelection(int amount){
+        List<ProgramCard> selection = null;
+        if (amount > activeDeck.size()) {
+            selection.addAll(activeDeck);
+            activeDeck.clear();
+            activeDeck.addAll(discardDeck);
+            shuffleActive();
+            discardDeck.clear();
+        }
+        for (int i = 0; i < amount - selection.size(); i++){
+            selection.add(activeDeck.get(i));
+        }
+        return selection;
+    }
+
+    // select card
+    public ProgramCard selectCard(ProgramCard card){
+
+        return null;
+    }
+    //
+
 
 }
