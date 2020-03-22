67dc05035dc7be93c385c963f2a632ce4a4593cb
Alvar <alvarhonsi@gmail.com>
2020-03-22 15:13:36 +0100

    unit tests

--- src/test/java/inf112/core/map/TileIdTest.java
+++ src/test/java/inf112/core/map/TileIdTest.java
@@ -1,13 +1,9 @@
 package inf112.core.map;
 
 import com.badlogic.gdx.math.Vector2;
-import inf112.core.tile.Attributes;
-import inf112.core.tile.TileId;
-import inf112.core.tile.WallTile;
-import org.junit.Before;
+import inf112.core.player.Direction;
+import inf112.core.tile.*;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
@@ -19,62 +15,91 @@ public class TileIdTest {
     public void getTileIdReturnsCorrectTileId() {
         assertEquals(TileId.WALL_SOUTH, TileId.getTileId(29));
         assertEquals(TileId.WALL_NORTH_EAST, TileId.getTileId(16));
+        for(TileId id : TileId.values()) {
+
+        }
     }
 
     @Test
-    public void WALL_SOUTHShouldHaveAttributesSOUTH(){
-        Attributes attributes = TileId.WALL_SOUTH.getAttributes().get(0);
-        assertEquals(attributes, Attributes.SOUTH);
+    public void WALL_SOUTHShouldHaveDirectionSOUTH(){
+        Direction dir = TileId.WALL_SOUTH.getFacingDirections().get(0);
+        assertEquals(dir, Direction.SOUTH);
     }
 
     @Test
-    public void WALL_NORTHShouldHaveAttributesNORTH(){
-        Attributes attributes = TileId.WALL_NORTH.getAttributes().get(0);
-        assertEquals(attributes, Attributes.NORTH);
+    public void WALL_NORTHShouldHaveDirectionNORTH(){
+        Direction dir = TileId.WALL_NORTH.getFacingDirections().get(0);
+        assertEquals(dir, Direction.NORTH);
     }
 
     @Test
-    public void WALL_EASTShouldHaveAttributesEAST(){
-        Attributes attributes = TileId.WALL_EAST.getAttributes().get(0);
-        assertEquals(attributes, Attributes.EAST);
+    public void WALL_EASTShouldHaveDirectionEAST(){
+        Direction dir = TileId.WALL_EAST.getFacingDirections().get(0);
+        assertEquals(dir, Direction.EAST);
     }
 
     @Test
-    public void WALL_WESTShouldHaveAttributesWEST(){
-        Attributes attributes = TileId.WALL_WEST.getAttributes().get(0);
-        assertEquals(attributes, Attributes.WEST);
+    public void WALL_WESTShouldHaveDirectionWEST(){
+        Direction dir = TileId.WALL_WEST.getFacingDirections().get(0);
+        assertEquals(dir, Direction.WEST);
     }
 
     @Test
-    public void WALL_SOUTH_WESTShouldHaveAttributesSOUTHAndWEST() {
-        List<Attributes> attributes = TileId.WALL_SOUTH_WEST.getAttributes();
-        for(Attributes attribute : attributes)
-            assert(attribute.equals(Attributes.WEST) || attribute.equals(Attributes.SOUTH));
+    public void WALL_SOUTH_WESTShouldHaveDirectionsSOUTHAndWEST() {
+        for(Direction dir : TileId.WALL_SOUTH_WEST.getFacingDirections())
+            assert(dir.equals(Direction.SOUTH) || dir.equals(Direction.WEST));
     }
 
     @Test
     public void WALL_NORTH_WESTShouldHaveAttributesNORTHAndWEST() {
-        List<Attributes> attributes = TileId.WALL_NORTH_WEST.getAttributes();
-        for(Attributes attribute : attributes)
-            assert(attribute.equals(Attributes.WEST) || attribute.equals(Attributes.NORTH));
+        for(Direction dir : TileId.WALL_NORTH_WEST.getFacingDirections())
+            assert(dir.equals(Direction.NORTH) || dir.equals(Direction.WEST));
     }
 
     @Test
     public void WALL_NORTH_EASTShouldHaveAttributesNORTHAndEAST() {
-        List<Attributes> attributes = TileId.WALL_NORTH_EAST.getAttributes();
-        for(Attributes attribute : attributes)
-            assert(attribute.equals(Attributes.EAST) || attribute.equals(Attributes.NORTH));
+        for(Direction dir : TileId.WALL_NORTH_EAST.getFacingDirections())
+        assert(dir.equals(Direction.NORTH) || dir.equals(Direction.EAST));
     }
 
     @Test
     public void WALL_SOUTH_EASTShouldHaveAttributesSOUTHAndEAST() {
-        List<Attributes> attributes = TileId.WALL_SOUTH_EAST.getAttributes();
-        for(Attributes attribute : attributes)
-            assert(attribute.equals(Attributes.EAST) || attribute.equals(Attributes.SOUTH));
+        for(Direction dir : TileId.WALL_SOUTH_EAST.getFacingDirections())
+            assert(dir.equals(Direction.SOUTH) || dir.equals(Direction.EAST));
     }
 
     @Test
     public void instantiateOnTileIdWallsShouldReturnAWallTileObject() {
         assertThat(TileId.WALL_NORTH.instantiate(new Vector2(1, 1)), instanceOf(WallTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdConveyorsShouldReturnAConveyorTileObject() {
+        assertThat(TileId.CONVEYOR_EAST_TO_WEST.instantiate(new Vector2(1, 1)), instanceOf(ConveyorTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdJunctionsShouldReturnAJunctionTileObject() {
+        assertThat(TileId.CONVEYOR_JUNCTION_NORTH_AND_SOUTH_TO_WEST.instantiate(new Vector2(1, 1)), instanceOf(JunctionTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdLasersShouldReturnALaserCannonTileObject() {
+        assertThat(TileId.LASER_WALL_EAST.instantiate(new Vector2(1, 1)), instanceOf(LaserCannonTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdSpawnsShouldReturnASpawnTileObject() {
+        assertThat(TileId.SPAWN_PLAYER1.instantiate(new Vector2(1, 1)), instanceOf(SpawnTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdFlagsShouldReturnAFlagTileObject() {
+        assertThat(TileId.FLAG_1.instantiate(new Vector2(1, 1)), instanceOf(FlagTile.class));
+    }
+
+    @Test
+    public void instantiateOnTileIdLasersShouldReturnALaserTileObject() {
+        assertThat(TileId.SINGLE_LASER_VERTICAL.instantiate(new Vector2(1, 1)), instanceOf(LaserTile.class));
     }
 }
