b738f1d9b555b75441846a1735fac90521ab28ac
Alvar <alvarhonsi@gmail.com>
2020-03-27 12:15:06 +0100

    reformat

--- src/main/java/inf112/core/game/RoundHandler.java
+++ src/main/java/inf112/core/game/RoundHandler.java
@@ -2,13 +2,13 @@ package inf112.core.game;
 
 import com.badlogic.gdx.math.Vector2;
 import inf112.core.board.GameBoard;
+import inf112.core.movement.MovementHandler;
 import inf112.core.player.Player;
 import java.util.List;
 
 public class RoundHandler {
     MainGame game;
-    GameBoard board;
-    List<Player> players;
+    MovementHandler movementHandler;
 
     /**
      * Class that handles a gameRound. Every round has several phases, and every phase has different actions.
@@ -17,9 +17,11 @@ public class RoundHandler {
      */
     public RoundHandler(MainGame game, List<Player> players) {
         this.game = game;
-        this.board = game.getBoard();
-        this.players = players;
+        this.movementHandler = game.getMovementHandler();
     }
 
+    private void runPhaseOne() {
+        movementHandler.runConveyors();
+    }
 
 }
